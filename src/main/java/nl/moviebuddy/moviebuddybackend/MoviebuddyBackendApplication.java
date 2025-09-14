a38ff226167819d095d94e7cfca2c415bde87461
Dominic-Schwartz <schwartzdominic@gmail.com>
2025-09-14 16:05:08 +0200

    Initial commit

---
+++ src/main/java/nl/moviebuddy/moviebuddybackend/MoviebuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.moviebuddy.moviebuddybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebuddyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviebuddyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/moviebuddy/moviebuddybackend/MoviebuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.moviebuddy.moviebuddybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebuddyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
