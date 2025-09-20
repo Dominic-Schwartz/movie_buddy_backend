b06198dbe19e6e4893019091fb4d76e6c36c2047
Dominic-Schwartz <schwartzdominic@gmail.com>
2025-09-20 14:42:13 +0200

    chore: add seed data for genres

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into genres (name, description) values
+                                           ('Action','Fast-paced, stunts, fights'),
+                                           ('Drama','Character-driven stories'),
+                                           ('Comedy','Light-hearted, humorous');
