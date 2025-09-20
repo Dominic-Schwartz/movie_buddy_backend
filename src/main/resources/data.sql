7637f6dd174da8c98ad727769ef6aab0626b3868
Dominic-Schwartz <schwartzdominic@gmail.com>
2025-09-20 14:56:55 +0200

    chore: add seed data for movies

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ insert into genres (name, description) values
                                            ('Action','Fast-paced, stunts, fights'),
                                            ('Drama','Character-driven stories'),
                                            ('Comedy','Light-hearted, humorous');
+
+insert into movies (title, description, release_year, genre_id)
+select 'Edge of Action', 'Prototype action movie', 2022, g.id from genres g where g.name = 'Action';
+
+insert into movies (title, description, release_year, genre_id)
+select 'Tears of Drama', 'Character-driven drama', 2021, g.id from genres g where g.name = 'Drama';
